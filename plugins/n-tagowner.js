58bbb5892202f847dc5bae02e48d4f9bcc04ca4f
AIMAN *͘⁴̅⁰͍⁴̵〆* <82744566+ai-man-123@users.noreply.github.com>
2022-02-17 16:36:15 +0800

    Update n-tagowner.js

--- plugins/n-tagowner.js
+++ plugins/n-tagowner.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-Sayanggg, Ada Yang Manggil Tuhhh😊
+Owner, Ada yang tag
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /@628882611841/i
+handler.customPrefix = /@60102810046/i
 handler.command = new RegExp
 
 module.exports = handler
